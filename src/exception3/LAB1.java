05cfe762e0311055c191c551f2a495e715c37267
Chaiyo <p50224.2014@gmail.com>
2023-03-31 01:27:28 +0700

    Exception part 1

---
+++ src/exception3/LAB1.java
@@ -0,0 +1,20 @@
+
+package exception3;
+
+public class LAB1 {
+    public static void main(String[] args) {
+        //  Exception
+        // System.out.println(1/0);  a.
+        // int[] list = new int[5];  b.
+        // System.out.println(list[5]); 
+        // String s="abc";           c.
+        // System.out.println(s.charAt(3));
+        // Object o=new Object();    d.
+        // String s=(String) o; 
+        // Object o=null;             e.
+        // o.toString();
+        // System.out.println(1.0/0);   f.
+
+        throw new ArithmeticException();
+    }
+}
